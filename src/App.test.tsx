a51bd1a60a2ba53d0cf06e597c2d0f8d0fab5c64
ponday-dev <ponday.dev+github@gmail.com>
2019-01-31 16:54:45 +0900

    update: test code

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import { store, history } from './redux';
 import App from './App';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    ReactDOM.render(
+        <Provider store={store}>
+            <ConnectedRouter history={history}>
+                <App />
+            </ConnectedRouter>
+        </Provider>,
+        div
+    );
     ReactDOM.unmountComponentAtNode(div);
 });
